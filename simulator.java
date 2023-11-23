0d5c695ac0ec041c3e5354429ba6529ed26e7268
VivekRanjan2002 <lit2020047@iiitl.ac.in>
2023-11-24 00:07:43 +0530

    Added: DiceRoller Method

--- simulator.java
+++ simulator.java
@@ -1,6 +1,10 @@
 public class simulator {
   
-   
+    // rolling of dice in particular turn
+    public static int DiceRoller() {
+        return (int) Math.ceil(Math.random() * 6);
+    }
+
     public static void main(String[] args) {
        final  int START_POS = 0;
        final   int FINAL_POS = 100;
