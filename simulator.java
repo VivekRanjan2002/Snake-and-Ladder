90edfc57dadb726a853d14aa6f5d495088c7af2d
VivekRanjan2002 <lit2020047@iiitl.ac.in>
2023-11-24 01:06:29 +0530

    Added: MovesCalculator Method

--- simulator.java
+++ simulator.java
@@ -1,13 +1,36 @@
 public class simulator {
-  
+
     // rolling of dice in particular turn
     public static int DiceRoller() {
         return (int) Math.ceil(Math.random() * 6);
     }
 
+
+    // no of moves to be made by player either 0 or move forward or move backward in a particular turn 
+    public static int MovesCalculator() {
+        
+        int option = (int) (Math.random() * 10) % 3;
+        System.out.println("option "+option);
+        int move = DiceRoller();
+        System.out.println("move "+move);
+        switch (option) {
+            case 0: // NO PLAY
+                move = 0;
+                break;
+
+            case 2:  // SNAKE
+                move *= -1;
+                break;
+            default: // LADDER
+                break;
+        }
+        return move;
+    }
+
     public static void main(String[] args) {
-       final  int START_POS = 0;
-       final   int FINAL_POS = 100;
-       
+         final int START_POS = 0;
+         final int FINAL_POS = 100;
+        
+
     }
 }
